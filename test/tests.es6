ee69606d6a2ad3634a8abd71a5113dca056b8e30
Lionel Chanson <github@lionelchanson.fr>
2016-09-21 22:57:23 +0200

    Import index module for test, babelify test.es6

--- test/tests.es6
+++ test/tests.es6
@@ -1,9 +1,15 @@
 import assert from 'assert';
+import result from '../index';
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+  describe('#isArray', function() {
+    it('should be [2,3,4] array', function() {
+      assert.equal([2,3,4], result);
     });
   });
 });
--- test/tests.js
+++ test/tests.js
@@ -4,12 +4,21 @@ var _assert = require('assert');
 
 var _assert2 = _interopRequireDefault(_assert);
 
+var _index = require('../index');
+
+var _index2 = _interopRequireDefault(_index);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       _assert2.default.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+  describe('#isArray', function () {
+    it('should be [2,3,4] array', function () {
+      _assert2.default.equal([2, 3, 4], _index2.default);
     });
   });
 });
